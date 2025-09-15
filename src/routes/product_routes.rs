9007b4a7a374808dd7a8472a1952f3b5cf7cac95
Jeannick Grondin <jeannick.grondin@willow.fr>
2025-09-15 09:31:06 +0400

    Ajout de l'authentification pour products routes

--- src/routes/product_routes.rs
+++ src/routes/product_routes.rs
@@ -12,8 +12,8 @@ pub fn create_product_routes() -> Router<SharedConnection> {
     let auth_layer = axum::middleware::from_fn(auth_middleware);
 
     Router::new()
-        .route("/products", get(get_all_products))
-        .route("/products/{id}", get(get_product))
+        .route("/products", get(get_all_products).route_layer(auth_layer.clone()))
+        .route("/products/{id}", get(get_product).route_layer(auth_layer.clone()))
         .route("/products", post(create_product).route_layer(auth_layer.clone()))
         .route("/products/{id}", put(update_product).route_layer(auth_layer.clone()))
         .route("/products/{id}", delete(delete_product_secure).route_layer(auth_layer))
